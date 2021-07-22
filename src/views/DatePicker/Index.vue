6c35fb13d3c0d8b6f0db0757f153786cf355858d
xiaoqiang <ext.xiaoqiang@jd.com>
2021-07-23 07:38:25 +0800

    引入.vscode

---
+++ src/views/DatePicker/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <DatePicker />
+  </div>
+</template>
+
+<script>
+import DatePicker from '@/views/DatePicker/components/DatePicker'
+export default {
+  name: 'MyComponentsIndex',
+  components: { DatePicker },
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/DatePicker/components/DatePicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>123</div>
+</template>
+<script>
+export default {
+  name: 'MyComponentsDatepicker',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
