d4c239ebea495c90344ceed0e30e08cfdbca7917
xiaoqiang <ext.xiaoqiang@jd.com>
2021-08-03 20:35:24 +0800

    create icon

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,7 @@
 import filters from '../../src/filters'
 import InitDemoBlock from './components/InitDemoBlock.vue'
 import Xq from '../../src/plugins'
-
+import './public/icon/icon.css'
 export default ({Vue}) => {
   Vue.use(Xq)
   Vue.component('InitDemoBlock', InitDemoBlock)
