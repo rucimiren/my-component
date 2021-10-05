1b3ea5c318630f1828889a291a0beac669696622
ext.xiaoqiang <ext.xiaoqiang@jd.com>
2021-10-05 15:07:27 +0800

    doc

--- docs/.vuepress/configs/sidebar/webDocumentEs6.js
+++ docs/.vuepress/configs/sidebar/webDocumentEs6.js
@@ -12,6 +12,42 @@ module.exports = {
           title: '02.let 和 const 命令',
           path: '/views/webDocument/ES6/02.let 和 const 命令',
         },
+        {
+          title: '03.变量的解构赋值',
+          path: '/views/webDocument/ES6/03.变量的解构赋值',
+        },
+        {
+          title: '04.字符串的扩展',
+          path: '/views/webDocument/ES6/04.字符串的扩展',
+        },
+        {
+          title: '05.字符串的新增方法',
+          path: '/views/webDocument/ES6/05.字符串的新增方法',
+        },
+        {
+          title: '06.正则的扩展',
+          path: '/views/webDocument/ES6/06.正则的扩展',
+        },
+        {
+          title: '07.数值的扩展',
+          path: '/views/webDocument/ES6/07.数值的扩展',
+        },
+        {
+          title: '08.函数的扩展',
+          path: '/views/webDocument/ES6/08.函数的扩展',
+        },
+        {
+          title: '09.数组的扩展',
+          path: '/views/webDocument/ES6/09.数组的扩展',
+        },
+        {
+          title: '10.对象的扩展',
+          path: '/views/webDocument/ES6/10.对象的扩展',
+        },
+        {
+          title: '11.对象的新增方法',
+          path: '/views/webDocument/ES6/11.对象的新增方法',
+        },
       ]
     }
   ]
