b820c0235c18caf552fb83327567ad645af6abcb
ext.xiaoqiang <ext.xiaoqiang@jd.com>
2021-11-02 21:44:23 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,49 @@ export default {
       this.$isCanvas(v, 'none'),
     )
 
-    const imgList = await loadImg({
+    /* const imgList = await loadImg({
       count: 18,
-      publicPath: 'http://yaru.vip:8080/images/family/0/',
+      publicPath: 'http://yaru.vip:8080/images/family/1/',
+    }) */
+    /* count: [
+        'IkATn1',
+        'IkAqAK',
+        'IkAOhD',
+        'IkAj9e',
+        'IkA70x',
+        'IkALtO',
+        'IkAv1H',
+        'IkAxcd',
+        'IkAzjA',
+        'IkECHP',
+        'IkEk4S',
+        'IkEpnI',
+        'IkE9Bt',
+        'IkEiAf',
+        'IkEFN8',
+        'IkEV3Q',
+        'IkEZcj',
+        'IkEE9g',
+      ], */
+    const imgList = await loadImg({
+      count: [
+        'IkKbfP',
+        'IkKOl8',
+        'IkKjOg',
+        'IkKX6S',
+        'IkKLSf',
+        'IkKxmQ',
+        'IkKzwj',
+        'IkMSTs',
+        'IkM9kn',
+        'IkMCYq',
+        'IkMFpV',
+        'IkMPf0',
+        'IkMklT',
+      ],
+      publicPath: 'https://z3.ax1x.com/2021/11/02/',
+      syncPublicPath: 'http://yaru.vip:8080/images/family/1/',
+      syncCount: ['13', '14', '15', '16', '17'],
     })
     this.imgList.push(...imgList)
   },
--- src/utils/businessUtils.js
+++ src/utils/businessUtils.js
@@ -1,19 +1,76 @@
 // 加载图片
-export function loadImg({ count, publicPath, format = 'jpg' }) {
+export function loadImg({
+  count,
+  publicPath,
+  format = 'jpg',
+  syncCount,
+  syncPublicPath,
+  syncFormat = 'jpg',
+}) {
   let result = []
-  for (let i = 0; i < count; i++) {
-    result.push(
-      new Promise(rs => {
-        const img = new Image()
-        img.onload = () => {
-          rs(img)
-        }
-        const src = `${publicPath}${i}.${format}`
-        img.src = src
-      }),
-    )
+  if (typeof count === 'number') {
+    for (let i = 0; i < count; i++) {
+      result.push(
+        new Promise(rs => {
+          const img = new Image()
+          img.onload = () => {
+            rs(img)
+          }
+          const src = `${publicPath}${i}.${format}`
+          img.src = src
+        }),
+      )
+    }
   }
-  return Promise.all(result)
+
+  if (Array.isArray(count)) {
+    for (let i = 0; i < count.length; i++) {
+      result.push(
+        new Promise(rs => {
+          const img = new Image()
+          img.onload = () => {
+            rs(img)
+          }
+          const src = `${publicPath}${count[i]}.${format}`
+          img.src = src
+        }),
+      )
+    }
+  }
+
+  let syncResult = []
+  if (syncCount) {
+    if (typeof syncCount === 'number') {
+      for (let i = 0; i < syncCount; i++) {
+        syncResult.push(
+          new Promise(rs => {
+            const img = new Image()
+            img.onload = () => {
+              rs(img)
+            }
+            const src = `${syncPublicPath}${i}.${syncFormat}`
+            img.src = src
+          }),
+        )
+      }
+    }
+    if (Array.isArray(syncCount)) {
+      for (let i = 0; i < syncCount.length; i++) {
+        syncResult.push(
+          new Promise(rs => {
+            const img = new Image()
+            img.onload = () => {
+              rs(img)
+            }
+            const src = `${syncPublicPath}${syncCount[i]}.${syncFormat}`
+            img.src = src
+          }),
+        )
+      }
+    }
+  }
+
+  return Promise.all([...result, ...syncResult])
 }
 
 // 是否显示特效
